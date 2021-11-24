35dc7af32f18fc3aaf483efb9dad13ac79dc63c4
ahmadaqil06 <ahmadaqil982@gmail.com>
2021-11-24 10:10:29 +0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        // membuat variabel dan Scanner
+
+        int nilai = 85;
+
+        switch(nilai){
+            case 90:
+                System.out.println("A");
+                break;
+            case 80:
+                System.out.println("B");
+                break;
+            case 70:
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("Nilai salah");
+        }
+    }
+
+
+}
+
+class nilaiDia{
+public static void main(String[] args){
+System.out.println("hello");
+}
+
+}
